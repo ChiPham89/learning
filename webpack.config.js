d0198001e386dd01730ba270b981f95b0ac80161
ChiPham89 <chi@codelink.io>
2020-03-25 18:27:13 +0700

    apply different boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,157 @@
+var webpack = require('webpack');
+var path = require('path');
+var package = require('./package.json');
+
+// variables
+var isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+var sourcePath = path.join(__dirname, './src');
+var outPath = path.join(__dirname, './build');
+
+// plugins
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
-  mode: "production",
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx"]
+  context: sourcePath,
+  entry: {
+    app: './main.tsx'
   },
-
+  output: {
+    path: outPath,
+    filename: isProduction ? '[contenthash].js' : '[hash].js',
+    chunkFilename: isProduction ? '[name].[contenthash].js' : '[name].[hash].js'
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    // Fix webpack's default behavior to not load packages with jsnext:main module
+    // (jsnext:main directs not usually distributable es6 format, but es6 sources)
+    mainFields: ['module', 'browser', 'main'],
+    alias: {
+      app: path.resolve(__dirname, 'src/app/')
+    }
+  },
   module: {
     rules: [
+      // .ts, .tsx
       {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
+        test: /\.tsx?$/,
         use: [
+          !isProduction && {
+            loader: 'babel-loader',
+            options: { plugins: ['react-hot-loader/babel'] }
+          },
+          'ts-loader'
+        ].filter(Boolean)
+      },
+      // css
+      {
+        test: /\.css$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
           {
-            loader: "ts-loader"
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              sourceMap: !isProduction,
+              importLoaders: 1,
+              localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-import')({ addDependencyTo: webpack }),
+                require('postcss-url')(),
+                require('postcss-preset-env')({
+                  /* use stage 2 features (defaults) */
+                  stage: 2
+                }),
+                require('postcss-reporter')(),
+                require('postcss-browser-reporter')({
+                  disabled: isProduction
+                })
+              ]
+            }
           }
         ]
       },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      // static assets
+      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.(a?png|svg)$/, use: 'url-loader?limit=10000' },
       {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
+        test: /\.(jpe?g|gif|bmp|mp3|mp4|ogg|wav|eot|ttf|woff|woff2)$/,
+        use: 'file-loader'
       }
     ]
   },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
+  optimization: {
+    splitChunks: {
+      name: true,
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          filename: isProduction ? 'vendor.[contenthash].js' : 'vendor.[hash].js',
+          priority: -10
+        }
+      }
+    },
+    runtimeChunk: true
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+      DEBUG: false
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[hash].css',
+      disable: !isProduction
+    }),
+    new HtmlWebpackPlugin({
+      template: 'assets/index.html',
+      minify: {
+        minifyJS: true,
+        minifyCSS: true,
+        removeComments: true,
+        useShortDoctype: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true
+      },
+      append: {
+        head: `<script src="//cdn.polyfill.io/v3/polyfill.min.js"></script>`
+      },
+      meta: {
+        title: package.name,
+        description: package.description,
+        keywords: Array.isArray(package.keywords) ? package.keywords.join(',') : undefined
+      }
+    })
+  ],
+  devServer: {
+    contentBase: sourcePath,
+    hot: true,
+    inline: true,
+    historyApiFallback: {
+      disableDotRule: true
+    },
+    stats: 'minimal',
+    clientLogLevel: 'warning'
+  },
+  // https://webpack.js.org/configuration/devtool/
+  devtool: isProduction ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+  node: {
+    // workaround for webpack-dev-server issue
+    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
+    fs: 'empty',
+    net: 'empty'
   }
 };
